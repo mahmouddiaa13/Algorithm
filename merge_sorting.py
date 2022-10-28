a00a34e79e919247d916c1e22ab036b5857a29b3
mahmouddiaa13 <mahmouddiaa13@gmail.com>
2022-10-28 17:34:56 +0200

    function to sort integers using merge sort algorithm

---
+++ merge_sorting.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+
+        left_arr = arr[:mid]
+        right_arr = arr[mid:]
+
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        i = j = k =0
+
+        while(i < len(left_arr) and j < len(right_arr)):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                k += 1
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                k += 1
+                j += 1
+
+        while i < len(left_arr):
+                arr[k] = left_arr[i]
+                i += 1
+                k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+
+if __name__ == "__main__":
+    arr = [9, 5, 1, 4, 3]
+    merge_sort(arr)
+    print(arr)
