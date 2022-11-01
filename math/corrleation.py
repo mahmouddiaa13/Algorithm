e127ab7d1e8cc1c55992e51c4707740735cb9284
mahmouddiaa13 <mahmouddiaa13@gmail.com>
2022-11-01 10:15:40 +0200

    typo correction

--- math/corrleation.py
+++ math/corrleation.py
@@ -1,6 +1,7 @@
 import math
 
-def get_corrleation():
+
+def get_correlation():
     n = int(input("n = ?? \n"))
     sigma_xy = sigma_x_sqr = sigma_y_sqr = sigma_x = sigma_y = 0
     for i in range(0, n):
@@ -11,19 +12,20 @@ def get_corrleation():
 
         sigma_x_sqr += (x ** 2)
         sigma_y_sqr += (y ** 2)
-        
+
         sigma_x += x
         sigma_y += y
 
     n_sigma_xy = n * sigma_xy
     sigma_x_sigma_y = sigma_x * sigma_y
     numerator = n_sigma_xy - sigma_x_sigma_y
-    
-    a = math.sqrt(n *sigma_x_sqr - sigma_x ** 2)
-    b = math.sqrt(n *sigma_y_sqr - sigma_y ** 2)
+
+    a = math.sqrt(n * sigma_x_sqr - sigma_x ** 2)
+    b = math.sqrt(n * sigma_y_sqr - sigma_y ** 2)
     denominator = a * b
 
-    return numerator/denominator
+    return numerator / denominator
+
 
 if __name__ == "__main__":
-    print(get_corrleation())
+    print(get_correlation())
